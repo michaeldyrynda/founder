bcf012b5c39afaf963abc1a386b6f12df881a2ce
Michael Dyrynda <michael@dyrynda.com.au>
2017-12-27 10:54:56 +1100

    add Sentry for error tracking

---
+++ config/sentry.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'dsn' => env('SENTRY_DSN'),
+
+    // capture release as git sha
+    'release' => file_exists($path = base_path('GITVERSION')) ? trim(file_get_contents($path)) : null,
+
+    // Capture bindings on SQL queries
+    'breadcrumbs.sql_bindings' => true,
+
+    // Capture default user context
+    'user_context' => true,
+);
