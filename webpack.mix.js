6bb57062384c3c15afcc957dbed8c8aa1cfcce6c
Michael Dyrynda <michael@dyrynda.com.au>
2018-01-27 15:00:13 +1030

    add laravel-mix-purgecss for compiling frontend assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 let mix = require('laravel-mix');
+require('laravel-mix-purgecss')();
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +13,5 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .purgeCss();
