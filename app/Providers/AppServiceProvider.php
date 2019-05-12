42954d215acd7b5a3c1ba9714486ea0de5009f5f
Michael Dyrynda <michael@dyrynda.com.au>
2019-05-13 09:22:26 +0930

    ignore package-provided migrations for Telescope, publish to app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Laravel\Horizon\Horizon;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Telescope\Telescope;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->isLocal()) {
             $this->app->register(TelescopeServiceProvider::class);
+
+            Telescope::ignoreMigrations();
         }        
     }
 }
