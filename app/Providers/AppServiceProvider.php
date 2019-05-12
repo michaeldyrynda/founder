1846e0df3ffd04e60cbcef23b479fd7937fc92b8
Michael Dyrynda <michael@dyrynda.com.au>
2019-05-13 09:20:14 +0930

    only register Telescope in local environment by default

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(TelescopeServiceProvider::class);
+        }        
     }
 }
