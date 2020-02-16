dffb18092a05e1a041be00d09e3ffe3d54b24f25
Michael Dyrynda <michael@dyrynda.com.au>
2020-02-16 20:48:53 +1030

    update telescope provider

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -2,11 +2,9 @@
 
 namespace App\Providers;
 
-use Laravel\Telescope\EntryType;
-use Laravel\Telescope\Telescope;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\IncomingEntry;
-use Laravel\Telescope\Contracts\EntriesRepository;
+use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
 
 class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
@@ -20,16 +18,39 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         // Telescope::night();
 
+        $this->hideSensitiveRequestDetails();
+
         Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->environment() == 'local') {
+            if ($this->app->environment('local')) {
                 return true;
             }
 
             return $entry->isReportableException() ||
+                   $entry->isFailedRequest() ||
                    $entry->isFailedJob() ||
                    $entry->isScheduledTask() ||
                    $entry->hasMonitoredTag();
         });
+    }
+
+    /**
+     * Prevent sensitive request details from being logged by Telescope.
+     *
+     * @return void
+     */
+    protected function hideSensitiveRequestDetails()
+    {
+        if ($this->app->environment('local')) {
+            return;
+        }
+
+        Telescope::hideRequestParameters(['_token']);
+
+        Telescope::hideRequestHeaders([
+            'cookie',
+            'x-csrf-token',
+            'x-xsrf-token',
+        ]);
     }
 
     /**
