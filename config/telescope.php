d0a107fdacee8e91bf6ebe6ef230038fe1f75b49
Michael Dyrynda <michael@dyrynda.com.au>
2020-07-28 01:41:31 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- config/telescope.php
+++ config/telescope.php
@@ -69,24 +69,24 @@ return [
     */
 
     'watchers' => [
-        Watchers\CacheWatcher::class => env('TELESCOPE_CACHE_WATCHER', true),
-        Watchers\CommandWatcher::class => env('TELESCOPE_COMMAND_WATCHER', true),
-        Watchers\DumpWatcher::class => env('TELESCOPE_DUMP_WATCHER', true),
-        Watchers\EventWatcher::class => env('TELESCOPE_EVENT_WATCHER', true),
-        Watchers\ExceptionWatcher::class => env('TELESCOPE_EXCEPTION_WATCHER', true),
-        Watchers\JobWatcher::class => env('TELESCOPE_JOB_WATCHER', true),
-        Watchers\LogWatcher::class => env('TELESCOPE_LOG_WATCHER', true),
-        Watchers\MailWatcher::class => env('TELESCOPE_MAIL_WATCHER', true),
-        Watchers\ModelWatcher::class => env('TELESCOPE_MODEL_WATCHER', true),
+        Watchers\CacheWatcher::class        => env('TELESCOPE_CACHE_WATCHER', true),
+        Watchers\CommandWatcher::class      => env('TELESCOPE_COMMAND_WATCHER', true),
+        Watchers\DumpWatcher::class         => env('TELESCOPE_DUMP_WATCHER', true),
+        Watchers\EventWatcher::class        => env('TELESCOPE_EVENT_WATCHER', true),
+        Watchers\ExceptionWatcher::class    => env('TELESCOPE_EXCEPTION_WATCHER', true),
+        Watchers\JobWatcher::class          => env('TELESCOPE_JOB_WATCHER', true),
+        Watchers\LogWatcher::class          => env('TELESCOPE_LOG_WATCHER', true),
+        Watchers\MailWatcher::class         => env('TELESCOPE_MAIL_WATCHER', true),
+        Watchers\ModelWatcher::class        => env('TELESCOPE_MODEL_WATCHER', true),
         Watchers\NotificationWatcher::class => env('TELESCOPE_NOTIFICATION_WATCHER', true),
 
         Watchers\QueryWatcher::class => [
             'enabled' => env('TELESCOPE_QUERY_WATCHER', true),
-            'slow' => 100,
+            'slow'    => 100,
         ],
 
-        Watchers\RedisWatcher::class => env('TELESCOPE_REDIS_WATCHER', true),
-        Watchers\RequestWatcher::class => env('TELESCOPE_REQUEST_WATCHER', true),
+        Watchers\RedisWatcher::class    => env('TELESCOPE_REDIS_WATCHER', true),
+        Watchers\RequestWatcher::class  => env('TELESCOPE_REQUEST_WATCHER', true),
         Watchers\ScheduleWatcher::class => env('TELESCOPE_SCHEDULE_WATCHER', true),
     ],
 ];
