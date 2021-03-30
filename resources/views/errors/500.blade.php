1d4a26d4c2f0cd76d0b140e654c9bc24662e8802
Michael Dyrynda <michael@dyrynda.com.au>
2021-03-30 13:05:48 +1030

    Update 500 view

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,22 +1,23 @@
-@extends('errors::layout')
+@extends('errors::minimal')
 
-@section('title', 'Error')
+@section('title', __('Server Error'))
+@section('code', 500)
+@section('message', __('Server Error'))
 
-@section('message', 'Whoops, looks like something went wrong.')
-
-@if(app()->bound('sentry') && !empty(Sentry::getLastEventID()))
-    <div class="subtitle">Error ID: {{ Sentry::getLastEventID() }}</div>
-
-    <!-- Sentry JS SDK 2.1.+ required -->
-    <script src="https://cdn.ravenjs.com/3.3.0/raven.min.js"></script>
+@if(app()->bound('sentry') && ! blank(Sentry::getLastEventID()))
+    <script
+        src="https://browser.sentry-cdn.com/6.2.3/bundle.min.js"
+        integrity="sha384-n6TNefxJMUTqJauZtoDyDhAs5Ng0VzcMTy0/afmyZoVZpaQ2clYR1LBa4SqhVySs"
+        crossorigin="anonymous"
+    ></script>
 
     <script>
-        Raven.showReportDialog({!! json_encode([
+        Sentry.init({!! json_encode([
             'eventId' => Sentry::getLastEventID(),
             'dsn' => config('sentry.public_dsn'),
             'user' => [
-                'name' => optional(auth()->user())->name,
-                'email' => optional(auth()->user())->email,
+                'name' => auth()->user()?->name,
+                'email' => auth()->user()?->email,
             ],
         ]) !!});
     </script>
