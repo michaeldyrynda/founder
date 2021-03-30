76de9c66616312312e27ab3df8f2fd0257e9d4bf
Michael Dyrynda <michael@dyrynda.com.au>
2021-03-30 13:05:38 +1030

    Rename and import UserController

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class UsersController extends Controller
+class UserController extends Controller
 {
     public function __invoke(Request $request)
     {
