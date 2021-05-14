adb287a5655639c7662e8ce5d8b8d43d1cc72805
Michael Dyrynda <michael@dyrynda.com.au>
2021-05-14 14:36:31 +0930

    remove default model, unguard in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Telescope\Telescope;
 
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        Model::unguard();
+
         if ($this->app->isLocal()) {
             $this->app->register(TelescopeServiceProvider::class);
 
