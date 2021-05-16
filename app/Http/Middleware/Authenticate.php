30932843ce4c1c2b5a9b3cf68cb1f1ca1e475a69
Michael Dyrynda <michael@dyrynda.com.au>
2021-05-17 09:23:40 +0930

    Update base middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -10,10 +10,13 @@ use Illuminate\Support\Facades\Auth;
 class RedirectIfAuthenticated
 {
     public function handle(Request $request, Closure $next, ...$guards)
-    public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+        $guards = empty($guards) ? [null] : $guards;
+
+        foreach ($guards as $guard) {
+            if (Auth::guard($guard)->check()) {
+                return redirect(RouteServiceProvider::HOME);
+            }
         }
 
         return $next($request);
