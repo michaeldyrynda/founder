98030c07c41387449fa59a14f1b761389882843e
Michael Dyrynda <michael@dyrynda.com.au>
2021-05-20 11:42:23 +0930

    add rector, update rector config

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\PostRector\Rector\NameImportingPostRector;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -31,8 +32,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters->set(Option::ENABLE_CACHE, true);
 
-    $parameters->set(Option::SETS, [
-        SetList::PHP_80,
-        SetList::PHPUNIT_91,
-    ]);
+    $containerConfigurator->import(SetList::PHP_80);
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_91);
 };
