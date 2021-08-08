cfd9f9eca0fa9e950add38f0156241e8b2ad936f
Michael Dyrynda <michael@dyrynda.com.au>
2021-08-09 09:15:52 +0930

    Use CarbonImmutable by default

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Telescope\Telescope;
 
@@ -11,6 +13,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         Model::unguard();
+
+        Date::use(CarbonImmutable::class);
 
         if ($this->app->isLocal()) {
             $this->app->register(TelescopeServiceProvider::class);
