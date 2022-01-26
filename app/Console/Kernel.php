945bd25386e51aa79a93b39a0523ba713c7ee7c2
Michael Dyrynda <michael@dyrynda.com.au>
2022-01-26 15:04:05 +1030

    Laravel 9.x Shift (#57)
    * Move `resources/lang` folder
    
    * Streamline `$commands` property
    
    * Replace deprecated `HEADER_X_FORWARDED_ALL` constant
    
    * Upgrade to Flysystem 3.0
    
    * Shift core files
    
    * Convert `optional()` to nullsafe operator
    
    * Use Faker methods
    
    * Remove unnecessary `$model` property
    
    * Shift config files
    
    * Default config files
    
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them and merged your true customizations -
    where ENV variables may not be used.
    
    * Bump Laravel dependencies
    
    * Use `<env>` tags for configuration
    
    `<env>` tags have a lower precedence than system environment variables making it easier to overwrite PHPUnit configuration values in additional environments, such a CI.
    
    Review this blog post for more details on configuration precedence when testing Laravel: https://jasonmccreary.me/articles/laravel-testing-configuration-precedence/
    
    * Shift cleanup
    
    * Update default language files for Laravel 9.x
    
    * remove remnants of Sentry
    
    * update dependencies for Laravel 9.x
    
    * remove check-security; enlightn was removed due to no L9 support
    
    * remove dependencies that aren't used on every project
    
    Co-authored-by: Shift <shift@laravelshift.com>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,10 +7,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [
-        //
-    ];
-
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,21 +2,32 @@
 
 namespace Database\Factories;
 
-use App\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
-    protected $model = User::class;
-
     public function definition(): array
     {
         return [
-            'name'           => $this->faker->name,
-            'email'          => $this->faker->unique()->safeEmail,
+            'name'           => $this->faker->name(),
+            'email'          => $this->faker->unique()->safeEmail(),
             'password'       => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
             'remember_token' => Str::random(10),
         ];
+    }
+
+    /**
+     * Define the model's unverified state.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
     }
 }
