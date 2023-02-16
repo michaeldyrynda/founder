ad939f4cdb1d911f66c9d8792db9faa5075503f0
Michael Dyrynda <michael@dyrynda.com.au>
2023-02-16 12:27:40 +1030

    normalise authenticate middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,15 +3,12 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
-    protected function redirectTo($request): ?string
+    protected function redirectTo(Request $request): ?string
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
-
-        return null;
+        return $request->expectsJson() ? null : route('login');
     }
 }
